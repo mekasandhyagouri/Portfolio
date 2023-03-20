673f1b93e1848c4818e810475f9936e2a165f1b9
mekasandhyagouri <sandychowdary2817@gmail.com>
2023-03-21 00:42:17 +0530

    fixed raw github

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,8 +8,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-"https://raw.githubusercontent.com/mekasandhyagouri/Portfolio/main/src/Assets/Sandhya_resume.pdf"
+const resumeLink = pdf
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
   
